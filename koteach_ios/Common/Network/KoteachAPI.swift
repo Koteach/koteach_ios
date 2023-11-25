db7e425aebfdcf2088d33edf968c3c482a371f7b
johnhur <johnjeongukhur@gmail.com>
2023-11-25 14:33:24 +0900

    feature: setup network, koteach api

--- koteach_ios/Common/Network/KoteachAPI.swift
+++ koteach_ios/Common/Network/KoteachAPI.swift
@@ -9,10 +9,10 @@ import Foundation
 import Alamofire
 import Combine
 
-enum TodoAPI {
-    static let baseURL = URL(string: "http://127.0.0.1:8000")!
+enum KoteachAPI {
+    static let baseURL = URL(string: "http://18.133.26.124")!
     
-    static let localURL = URL(string: "http://127.0.0.1:8000")!
+    static let localURL = URL(string: "http://18.133.26.124")!
     
     static func headers() -> HTTPHeaders {
         return [
@@ -25,64 +25,62 @@ enum TodoAPI {
     
 }
 
-//extension TodoAPI {
-//    static private func makeRequest<T: Decodable>(_ url: URL, method: HTTPMethod, parameters: Parameters?, encoding: ParameterEncoding, headers: HTTPHeaders?) -> Observable<T> {
-//        return Observable.create { observer in
-//            let request = AF.request(url, method: method, parameters: parameters, encoding: encoding, headers: headers)
-//                .validate()
-//                .responseDecodable(of: T.self) { response in
-//                    switch response.result {
-//                    case .success(let value):
-//                        observer.onNext(value)
-//                        observer.onCompleted()
-//                    case .failure(let error):
-//                        observer.onError(error)
-//                    }
-//                }
-//            return Disposables.create {
-//                request.cancel()
-//            }
-//        }
-//    }
-//    
-//    
-//    static func get<T: Decodable>(_ url: URL) -> Observable<T> {
-//        return makeRequest(url, method: .get, parameters: nil, encoding: URLEncoding.default, headers: TodoAPI.headers())
-//    }
-//    
-//    static func get<T: Decodable, P: Encodable>(_ url: URL, parameters: P) -> Observable<T> {
-//        return makeRequest(url, method: .get, parameters: parameters as? Parameters, encoding: URLEncoding.queryString, headers: TodoAPI.headers())
-//    }
-//    
-//    static func post<T: Decodable>(_ url: URL) -> Observable<T> {
-//        return makeRequest(url, method: .post, parameters: nil, encoding: URLEncoding.default, headers: TodoAPI.headers())
-//    }
-//    
-//    static func post<T: Decodable, P: Encodable>(_ url: URL, jsonParameters: P) -> Observable<T> {
-//        return makeRequest(url, method: .post, parameters: jsonParameters as? Parameters, encoding: JSONEncoding.prettyPrinted, headers: TodoAPI.headers())
-//    }
-//    
-//    static func post<T: Decodable>(_ url: URL, parameters: [String: String]) -> Observable<T> {
-//        return makeRequest(url, method: .post, parameters: parameters, encoding: URLEncoding.httpBody, headers: TodoAPI.headers())
-//    }
-//    
-//    static func post<T: Decodable>(_ url: URL, parameters: [String: Any]) -> Observable<T> {
-//        return makeRequest(url, method: .post, parameters: parameters, encoding: URLEncoding.queryString, headers: TodoAPI.headers())
-//    }
-//    
-//    static func post<T: Decodable, P: Encodable>(_ url: URL, parameters: P) -> Observable<T> {
-//        return makeRequest(url, method: .post, parameters: parameters as? Parameters, encoding: URLEncoding.httpBody, headers: nil)
-//    }
-//    
-//    static func put<T: Decodable>(_ url: URL, parameters: [String: Any]) -> Observable<T> {
-//        return makeRequest(url, method: .put, parameters: parameters, encoding: URLEncoding.queryString, headers: TodoAPI.headers())
-//    }
-//    
-//    static func delete<T: Decodable>(_ url: URL) -> Observable<T> {
-//        return makeRequest(url, method: .delete, parameters: nil, encoding: URLEncoding.default, headers: TodoAPI.headers())
-//    }
-//    
-//}
+extension KoteachAPI {
+    static private func makeRequest<T: Decodable>(_ url: URL, method: HTTPMethod, parameters: Parameters?, encoding: ParameterEncoding, headers: HTTPHeaders?) -> AnyPublisher<T, Error> {
+        return Future<T, Error> { promise in
+            let request = AF.request(url, method: method, parameters: parameters, encoding: encoding, headers: headers)
+                .validate()
+                .responseDecodable(of: T.self) { response in
+                    switch response.result {
+                    case .success(let value):
+                        promise(.success(value))
+                    case .failure(let error):
+                        promise(.failure(error))
+                    }
+                }
+            
+            request.cancel()
+        }.eraseToAnyPublisher()
+    }
+
+    
+    static func get<T: Decodable>(_ url: URL) -> AnyPublisher<T, Error> {
+        return makeRequest(url, method: .get, parameters: nil, encoding: URLEncoding.default, headers: KoteachAPI.headers())
+    }
+    
+    static func get<T: Decodable, P: Encodable>(_ url: URL, parameters: P) -> AnyPublisher<T, Error> {
+        return makeRequest(url, method: .get, parameters: parameters as? Parameters, encoding: URLEncoding.queryString, headers: KoteachAPI.headers())
+    }
+    
+    static func post<T: Decodable>(_ url: URL) -> AnyPublisher<T, Error> {
+        return makeRequest(url, method: .post, parameters: nil, encoding: URLEncoding.default, headers: KoteachAPI.headers())
+    }
+    
+    static func post<T: Decodable, P: Encodable>(_ url: URL, jsonParameters: P) -> AnyPublisher<T, Error> {
+        return makeRequest(url, method: .post, parameters: jsonParameters as? Parameters, encoding: JSONEncoding.prettyPrinted, headers: KoteachAPI.headers())
+    }
+    
+    static func post<T: Decodable>(_ url: URL, parameters: [String: String]) -> AnyPublisher<T, Error> {
+        return makeRequest(url, method: .post, parameters: parameters, encoding: URLEncoding.httpBody, headers: KoteachAPI.headers())
+    }
+    
+    static func post<T: Decodable>(_ url: URL, parameters: [String: Any]) -> AnyPublisher<T, Error> {
+        return makeRequest(url, method: .post, parameters: parameters, encoding: URLEncoding.queryString, headers: KoteachAPI.headers())
+    }
+    
+    static func post<T: Decodable, P: Encodable>(_ url: URL, parameters: P) -> AnyPublisher<T, Error> {
+        return makeRequest(url, method: .post, parameters: parameters as? Parameters, encoding: URLEncoding.httpBody, headers: nil)
+    }
+    
+    static func put<T: Decodable>(_ url: URL, parameters: [String: Any]) -> AnyPublisher<T, Error> {
+        return makeRequest(url, method: .put, parameters: parameters, encoding: URLEncoding.queryString, headers: KoteachAPI.headers())
+    }
+    
+    static func delete<T: Decodable>(_ url: URL) -> AnyPublisher<T, Error> {
+        return makeRequest(url, method: .delete, parameters: nil, encoding: URLEncoding.default, headers: KoteachAPI.headers())
+    }
+    
+}
 
 enum CustomErrorType: Error {
     case networkError
