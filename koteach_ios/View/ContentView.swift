00a7b23c48502cf31f77c14a9363b5140ae97924
johnhur <johnjeongukhur@gmail.com>
2023-11-25 21:26:18 +0900

    fix: add Tabview resolve: merge conflicts

--- koteach_ios/View/ContentView.swift
+++ koteach_ios/View/ContentView.swift
@@ -8,113 +8,60 @@
 
 import SwiftUI
 
-struct AcademyReviewView: View {
+struct ContentView: View {
+    
     var body: some View {
-        NavigationStack {
-            HStack {
-                Spacer().frame(width: 20)
-                AcademySearchandFilterBar()
-                Spacer().frame(width: 20)
-            }
+        
+        TabView {
+            AcademyReviewView()
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Hagwon")
+                }
             
-            ScrollView {
-                LazyVStack (spacing: 20) {
-                    ForEach (0 ... 10, id: \.self) { listing in
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 30)
-                                .foregroundColor(.white)
-                                .overlay {
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .stroke(lineWidth: 0.5)
-                                }
+            VStack {
+                AcademySearchandFilterBar()
+                
+                ScrollView {
+                    LazyVStack (spacing: 30) {
+                        ForEach (0 ... 10, id: \.self) { listing in Rectangle()
+                                .frame(height: 400)
+                                .foregroundColor(.green)
+                                .clipShape(RoundedRectangle(cornerRadius: 100))
                             
-                            //Academy details
-                            
-                            HStack() {
-                                // Details
-                                Spacer().frame(width: 12)
-                                
-                                VStack(alignment: .leading) {
-                                    Spacer().frame(height: 12)
-                                    
-                                    Text("Hagwon Name")
-                                        .lato(family: .Bold, size: 15)
-                                        .foregroundColor(.black)
-                                    
-                                    HStack(spacing: 0) {
-                                        
-                                        Text("Location, City")
-                                            .foregroundStyle(.gray)
-                                            .lato(family: .Light, size: 10)
-                                        
-                                        Spacer().frame(width: 10)
-                                        
-                                        Image(systemName: "star.fill")
-                                            .resizable()
-                                            .frame(width: 12, height: 12)
-                                        
-                                        Spacer().frame(width: 1)
-                                        
-                                        Text("4.5")
-                                            .lato(family: .Light, size: 10)
-                                    }
-                                    Spacer().frame(height: 10)
-                                    
-                                    HStack {
-                                        Text("Hagwon information Hagwon information Hagwon information Hagwon information Hagwon information Hagwon information Hagwon information Hagwon information")
-                                        
-                                            .lato(family: .Regular, size: 12)
-                                            .foregroundColor(.black)
-                                        
-                                        Spacer()
-                                    }
-                                    
-                                    Spacer().frame(height: 12)
-                                    
-                                    HStack {
-                                        Spacer().frame(width: 10)
-                                        
-                                        Image(systemName: "hand.thumbsup")
-                                            .resizable()
-                                            .frame(width: 12, height: 12)
-                                        
-                                        Text("Like")
-                                            .lato(family: LatoFamily.Regular, size: 12)
-                                        
-                                        
-//                                        Spacer().frame(width: 61)
-                                        Spacer()
-                                        
-                                        Image(systemName: "message")
-                                        Text("Comment")
-                                            .lato(family: LatoFamily.Regular, size: 12)
-                                        
-                                        
-//                                        Spacer().frame(width: 61)
-                                        Spacer()
-                                        
-                                        Image(systemName: "bookmark")
-                                        Text("Save")
-                                            .lato(family: LatoFamily.Regular, size: 12)
-                                        
-                                        Spacer().frame(width: 10)
-
-                                    }
-                                    
-                                    Spacer().frame(height: 10)
-                                    
-                                }
-                                Spacer()
-                            }
                         }
-                        .padding()
-                        
                     }
+                    .padding()
                 }
             }
-            .padding()
+            .tabItem {
+                Image(systemName: "character.bubble")
+                Text("Chatbot")
+                
+            }
+            
+            VStack {
+                AcademySearchandFilterBar()
+                
+                ScrollView {
+                    LazyVStack (spacing: 30) {
+                        ForEach (0 ... 10, id: \.self) { listing in Rectangle()
+                                .frame(height: 400)
+                                .clipShape(RoundedRectangle(cornerRadius: 10))
+                                .foregroundColor(.red)
+                            
+                        }
+                    }
+                    .padding()
+                }
+            }
+            .tabItem {
+                Image(systemName: "dollarsign.square")
+                Text("Calculator")
+            }
         }
     }
+    
 }
 
 #Preview {
--- koteach_ios/View/TabbarView.swift
+++ koteach_ios/View/TabbarView.swift
@@ -8,26 +8,26 @@
 import SwiftUI
 
 
-struct ContentView : View {
-  var body: some View {
-    TabView {
-      Text("The First Tab")
-        .tabItem {
-          Image(systemName: "1.square.fill")
-          Text("First")
-        }
-      Text("Another Tab")
-        .tabItem {
-          Image(systemName: "2.square.fill")
-          Text("Second")
-        }
-      Text("The Last Tab")
-        .tabItem {
-          Image(systemName: "3.square.fill")
-          Text("Third")
-        }
-        .badge(10)
-    }
-    .font(.headline)
-  }
-}
+//struct ContentView : View {
+//  var body: some View {
+//    TabView {
+//      Text("The First Tab")
+//        .tabItem {
+//          Image(systemName: "1.square.fill")
+//          Text("First")
+//        }
+//      Text("Another Tab")
+//        .tabItem {
+//          Image(systemName: "2.square.fill")
+//          Text("Second")
+//        }
+//      Text("The Last Tab")
+//        .tabItem {
+//          Image(systemName: "3.square.fill")
+//          Text("Third")
+//        }
+//        .badge(10)
+//    }
+//    .font(.headline)
+//  }
+//}
